cea0a2367afe4f112fac1f024a0b9bd6aa2b0ff7
Harshit Raj <41303856+harshit-8080@users.noreply.github.com>
2023-07-07 00:39:25 +0530

    project setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BookIcon from "@mui/icons-material/Book";
+
+
+function Header() {
+    return (
+        <>
+            <BookIcon />
+            Google Keep
+        </>
+    )
+}
+
+export default Header;
